537e5900e21fee93c3eab70045caef91434ce223
jakubBone <jakub.bone1990@gmail.com>
2024-02-14 10:52:47 +0100

    Commit 3: Configured DecktopLauncher settings

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.SnakeGame;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
 		config.setTitle("SnakeGame");
+		config.setWindowedMode(800, 800);
+		config.setForegroundFPS(60);
+		config.useVsync(true);
 		new Lwjgl3Application(new SnakeGame(), config);
 	}
 }
