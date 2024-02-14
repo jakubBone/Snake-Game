f3735f6d17a0def91f91bb94e182a6d9742d87f8
jakubBone <jakub.bone1990@gmail.com>
2024-02-14 13:21:51 +0100

    Commit 6: Renamed assets. Added new Rectangle and OrtographicCamera obj

--- core/src/com/mygdx/game/SnakeGame.java
+++ core/src/com/mygdx/game/SnakeGame.java
@@ -4,41 +4,60 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
 
 public class SnakeGame extends ApplicationAdapter {
-
+	private SpriteBatch batch;
+	private Texture welcomeImage;
 	private Music snakeWelcome;
 	private Music snakeMove;
 	private Sound snakeGulp;
-	SpriteBatch batch;
-	Texture img;
+	private OrthographicCamera camera;
+	private Rectangle rectange;
 	
 	@Override
 	public void create () {
-		snakeWelcome = Gdx.audio.newMusic(Gdx.files.internal("snakeWelcome.mp3"));
-		snakeMove = Gdx.audio.newMusic(Gdx.files.internal("snakeMove.mp3"));
-		snakeGulp = Gdx.audio.newSound(Gdx.files.internal("snakeGulp.wav"));
+		welcomeImage = new Texture("welcomeImage.png");
 
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 800);
+
+		batch = new SpriteBatch();
+
+		snakeWelcome = Gdx.audio.newMusic(Gdx.files.internal("welcomeMusic.mp3"));
+		snakeMove = Gdx.audio.newMusic(Gdx.files.internal("snakeMoveSound.mp3"));
+		snakeGulp = Gdx.audio.newSound(Gdx.files.internal("snakeGulpSound.wav"));
 		snakeWelcome.setLooping(true);
 		snakeWelcome.play();
-		batch = new SpriteBatch();
-		img = new Texture("snake.png");
+
+
+		rectange = new Rectangle();
+		rectange.x = 800 / 2 - 64 / 2;
+		rectange.y = 20;
+		rectange.height = 64;
+		rectange.width = 64;
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(welcomeImage, rectange.x, rectange.y);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		welcomeImage.dispose();
 	}
 }
