e995d2c904c3567b72bd4c5f60e0fee955979810
jakubBone <jakub.bone1990@gmail.com>
2024-02-16 12:28:24 +0100

    Commit 10: Refacoted the SnakeGame class dividing the functionality between the new GameScreen and WelcomeScreen classes responsile for creating the two main game screens. Delated useless methods and variables and changed and addapt existing to the new scenario

---
+++ core/src/com/mygdx/game/WelcomeScreen.java
@@ -0,0 +1,53 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public class WelcomeScreen extends ScreenAdapter implements ApplicationListener {
+    private Texture welcomeImage;
+    private Music snakeWelcomeMusic;
+    private OrthographicCamera camera;
+    private Rectangle screen;
+    private SpriteBatch batch;
+
+    public void create() {
+        welcomeImage = new Texture("welcomeImage.png");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 800);
+
+        batch = new SpriteBatch();
+
+        snakeWelcomeMusic = Gdx.audio.newMusic(Gdx.files.internal("welcomeMusic.mp3"));
+        snakeWelcomeMusic.setLooping(true);
+        snakeWelcomeMusic.play();
+
+        // WelcomeImage
+        screen = new Rectangle();
+        screen.x = 0;
+        screen.y = 0;
+    }
+
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(welcomeImage, screen.x, screen.y);
+        batch.end();
+    }
+
+    public void dispose() {
+        welcomeImage.dispose();
+        batch.dispose();
+        snakeWelcomeMusic.dispose();
+    }
+}
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,18 +1,19 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.SnakeGame;
-
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("SnakeGame");
 		config.setWindowedMode(800, 800);
 		config.setForegroundFPS(60);
 		config.useVsync(true);
-		new Lwjgl3Application(new SnakeGame(), config);
 
+		new Lwjgl3Application(new WelcomeScreen(), config);
+		new Lwjgl3Application(new GameScreen(), config);
 	}
 }
